8bf39a2a9d705c17fd2262f4a7cdee65444bc0ac
Lele <nicolaslc.main@gmail.com>
2023-09-03 17:45:40 -0300

    Cambios en la conexión a la base de datos (servidor/local)

--- app/modelo/conexion.php
+++ app/modelo/conexion.php
@@ -1,13 +1,29 @@
 <?php
 
+$servidor = "localhost";
+
+// HOSTING DE SOSA
+// $usuario = "u761283263_beatcore";
+
+// $clave = "Beatcore2023";
+
+// $db_name = "u761283263_beatcore";
+
+// LOCAL
+$usuario = "root";
+
+$clave = "";
+
+$db_name = "beatcore";
+
 $con = mysqli_connect(
-  "localhost",
+  "$servidor",
   // Nombre de usuario
-  "u761283263_beatcore",
+  "$usuario",
   // Contrasenia
-  "Beatcore2023",
+  "$clave",
   // Nombre de base de datos
-  "u761283263_beatcore"
+  "$db_name"
 );
 
 
