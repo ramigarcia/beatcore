d0b1b8c52406b1df4bf1655e1778aa1569e98b30
ramigarcia <ramirogarciaweb@gmail.com>
2023-09-05 00:09:05 -0300

    Agregue las fuentes montserrat y nunito

--- app/componentes/header.php
+++ app/componentes/header.php
@@ -25,44 +25,26 @@ define("FOTO_PORTADA", "../../publico/img/foto_portada/");
       <li class="nav-lista-item">
         <!-- NOTIFICACIONES -->
         <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-            style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-            <path
-              d="M12 22a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22zm7-7.414V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v4.586l-1.707 1.707A.996.996 0 0 0 3 17v1a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-1a.996.996 0 0 0-.293-.707L19 14.586z">
-            </path>
-          </svg>
+        <i class='bx bxs-bell'></i>
         </a>
       </li>
       <li class="nav-lista-item">
         <!-- CHATS -->
         <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-            style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-            <path
-              d="M5 18v3.766l1.515-.909L11.277 18H16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h1zM4 8h12v8h-5.277L7 18.234V16H4V8z">
-            </path>
-            <path d="M20 2H8c-1.103 0-2 .897-2 2h12c1.103 0 2 .897 2 2v8c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2z">
-            </path>
-          </svg>
+        <i class='bx bx-chat'></i>
         </a>
       </li>
       <li class="nav-lista-item item-perfil">
         <!-- LINK AL PERFIL DEL USUARIO -->
         <a href="perfil.php?id_usuario=<?php echo $id_usuario; ?>">
-          <img src="<?= FOTO_PERFIL . datosUsuario($id_usuario, "foto_perfil")["foto_perfil"]; ?>"
-            width="20px">
+          <img src="<?= FOTO_PERFIL . datosUsuario($id_usuario, "foto_perfil")["foto_perfil"]; ?>">
         </a>
         <!-- FIN - LINK AL PERFIL -->
       </li>
       <!-- TRES PUNTOS (COMO LA HAMBURGUESITA) -->
       <li class="nav-lista-item">
         <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-            style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-            <path
-              d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z">
-            </path>
-          </svg>
+        <i class='bx bx-menu'></i>
         </a>
       </li>
       <ul class="lista-menu">
