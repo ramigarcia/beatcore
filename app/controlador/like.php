87e6a8326371c7ae3bc652f584bf369fcf961837
ramiGarcia <ramirogarciaweb@gmail.com>
2023-09-10 00:20:49 -0300

    Le di formato :D

--- app/controlador/like.php
+++ app/controlador/like.php
@@ -4,72 +4,72 @@ session_start();
 
 include("../modelo/conexion.php");
 
-    if(isset($_GET["id_publicacion"])){
+if (isset($_GET["id_publicacion"])) {
 
-        $id_publicacion = $_GET["id_publicacion"];
+  $id_publicacion = $_GET["id_publicacion"];
 
-        $id_usuario = $_SESSION["id_usuario"];
+  $id_usuario = $_SESSION["id_usuario"];
 
-        $query = "SELECT * FROM t_likes WHERE id_publicacion = '$id_publicacion' AND id_usuario = '$id_usuario'";
+  $query = "SELECT * FROM t_likes WHERE id_publicacion = '$id_publicacion' AND id_usuario = '$id_usuario'";
 
-        $dio_like = mysqli_num_rows(mysqli_query($con, $query));
+  $dio_like = mysqli_num_rows(mysqli_query($con, $query));
 
-        if($dio_like){
+  if ($dio_like) {
 
-            // SACAR EL LIKE
+    // SACAR EL LIKE
 
-            $query = "DELETE FROM t_likes WHERE id_usuario = '$id_usuario' AND id_publicacion = '$id_publicacion'";
+    $query = "DELETE FROM t_likes WHERE id_usuario = '$id_usuario' AND id_publicacion = '$id_publicacion'";
 
-            $res = mysqli_query($con, $query);
+    $res = mysqli_query($con, $query);
 
-        }else{
+  } else {
 
-            // DAR LIKE
+    // DAR LIKE
 
-            $query = "INSERT INTO t_likes(id_usuario, id_publicacion, fecha_like) VALUES('$id_usuario', '$id_publicacion', now())";
+    $query = "INSERT INTO t_likes(id_usuario, id_publicacion, fecha_like) VALUES('$id_usuario', '$id_publicacion', now())";
 
-            $res = mysqli_query($con, $query);
+    $res = mysqli_query($con, $query);
 
-        }
+  }
 
-        header('Location:' . getenv('HTTP_REFERER'));
+  header('Location:' . getenv('HTTP_REFERER'));
 
-    }else if(isset($_GET["id_comentario"])){
+} else if (isset($_GET["id_comentario"])) {
 
-        $id_comentario = $_GET["id_comentario"];
+  $id_comentario = $_GET["id_comentario"];
 
-        $id_publicacion_c = $_GET["id_publicacion_c"];
+  $id_publicacion_c = $_GET["id_publicacion_c"];
 
-        $id_usuario = $_SESSION["id_usuario"];
+  $id_usuario = $_SESSION["id_usuario"];
 
-        $query = "SELECT * FROM t_likes WHERE id_comentario = '$id_comentario' AND id_usuario = '$id_usuario'";
+  $query = "SELECT * FROM t_likes WHERE id_comentario = '$id_comentario' AND id_usuario = '$id_usuario'";
 
-        $dio_like = mysqli_num_rows(mysqli_query($con, $query));
+  $dio_like = mysqli_num_rows(mysqli_query($con, $query));
 
-        if($dio_like){
+  if ($dio_like) {
 
-            // SACAR EL LIKE
+    // SACAR EL LIKE
 
-            $query = "DELETE FROM t_likes WHERE id_usuario = '$id_usuario' AND id_comentario = '$id_comentario'";
+    $query = "DELETE FROM t_likes WHERE id_usuario = '$id_usuario' AND id_comentario = '$id_comentario'";
 
-            $res = mysqli_query($con, $query);
+    $res = mysqli_query($con, $query);
 
-        }else{
+  } else {
 
-            // DAR LIKE
+    // DAR LIKE
 
-            $query = "INSERT INTO t_likes(id_usuario, id_comentario, fecha_like) VALUES('$id_usuario', '$id_comentario', now())";
+    $query = "INSERT INTO t_likes(id_usuario, id_comentario, fecha_like) VALUES('$id_usuario', '$id_comentario', now())";
 
-            $res = mysqli_query($con, $query);
+    $res = mysqli_query($con, $query);
 
-        }
+  }
 
-        header('Location:' . getenv('HTTP_REFERER'));
+  header('Location:' . getenv('HTTP_REFERER'));
 
-    }else{
+} else {
 
-        header('Location:' . getenv('HTTP_REFERER'));
-        
-      }
+  header('Location:' . getenv('HTTP_REFERER'));
+
+}
 
 ?>
